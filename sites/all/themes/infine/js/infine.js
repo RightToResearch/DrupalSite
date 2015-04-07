979832f717febf9f3f141bfb52b320b495a99c67
Ed Baker <edwbaker@gmail.com>
2015-04-07 11:01:11 +0100

    Additional lsider.

--- sites/all/themes/infine/js/infine.js
+++ sites/all/themes/infine/js/infine.js
@@ -34,7 +34,11 @@ jQuery(document).ready(function ($) {
 
     $('#revolution-4 li:nth-child(1) > div').wrap('<a href="/attend" />');
     $('#revolution-4 li:nth-child(2) > div').wrap('<a href="/sponsor" />');
-    $('#revolution-4 li:nth-child(3) > div').wrap('<a href="/satellite />');
+    $('#revolution-4 li:nth-child(3) > div').wrap('<a href="/satellite" />');
+<<<<<<< HEAD
+    $('#revolution-4 li:nth-child(4) > div').wrap('<a href="/attend" />');
+=======
+>>>>>>> 41d96fe6c5af30b7085ec7e64281697ddf8f0344
 
 });
 
